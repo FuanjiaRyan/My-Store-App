5e13da8db7003bbf3434c13311508b4bc305845a
FuanjiaRyan <fuanjiaakemfua@gmail.com>
2025-10-04 16:59:17 +0100

    added banners in firestore

--- lib/views/screens/nav_screens/widgets/banner_widget.dart
+++ lib/views/screens/nav_screens/widgets/banner_widget.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:ryan_store_app/controllers/banner_controller.dart';
 
 class BannerWidget extends StatefulWidget {
  const BannerWidget({super.key});
@@ -9,40 +9,88 @@ class BannerWidget extends StatefulWidget {
 }
 
 class _BannerWidgetState extends State<BannerWidget> {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final BannerController _bannerController = BannerController();
+  // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  final List _bannerImage = [];
+  // final List _bannerImage = [];
+  //
+  // getBanners() {
+  //   return _firestore
+  //       .collection('banners')
+  //       .get()
+  //       .then((QuerySnapshot querySnapshot) {
+  //         querySnapshot.docs.forEach((doc) {
+  //           setState(() {
+  //             _bannerImage.add(doc['image']);
+  //           });
+  //         });
+  //   });
+  // }
 
-  getBanners() {
-    return _firestore
-        .collection('banners')
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-          querySnapshot.docs.forEach((doc) {
-            setState(() {
-              _bannerImage.add(doc['image']);
-            });
-          });
-    });
-  }
-
-  @override
-  void initState() {
-    getBanners();
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   getBanners();
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 140,
-      width: MediaQuery.of(context).size.width,
-      child: PageView.builder(
-        itemCount: _bannerImage.length,
-          itemBuilder: (context, index) {
-            return Image.network(_bannerImage[index]);
-          },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 170,
+        decoration: BoxDecoration(
+          color: Color(0xfff7f7f7),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 4,
+              blurRadius: 10,
+              offset: Offset(0,2),
+            ),
+          ],
+        ),
+        child: StreamBuilder<List<String>>(
+            stream: _bannerController.getBannerUrls(),
+            builder: (context, snapshot) {
+              if(snapshot.connectionState==ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(color: Colors.blueAccent,),
+                );
+              } else if(snapshot.hasError) {
+                return Icon(Icons.error);
+              } else if(!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Center(
+                  child: Text('No Banner available', style: TextStyle(fontSize: 18, color: Colors.grey),),
+                );
+              } else {
+                return Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    PageView.builder(
+                      itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Image.network(snapshot.data![index], fit: BoxFit.cover,);
+                        },
+                    )
+                  ],
+                );
+              }
+            },
+        ),
       ),
     );
+    // 
+    // SizedBox(
+    //   height: 140,
+    //   width: MediaQuery.of(context).size.width,
+    //   child: PageView.builder(
+    //     itemCount: _bannerImage.length,
+    //       itemBuilder: (context, index) {
+    //         return Image.network(_bannerImage[index]);
+    //       },
+    //   ),
+    // );
   }
 }
--- lib/views/screens/nav_screens/widgets/header_widget.dart
+++ lib/views/screens/nav_screens/widgets/header_widget.dart
@@ -5,102 +5,104 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.20,
-      child: Stack(
-        children: [
-          Image.asset(
-            'assets/images/banner.png',
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
-          Positioned(
-            left: 48,
-            top: 40,
-            child: SizedBox(
-              width: 250,
-              height: 50,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter text',
-                  hintStyle: TextStyle(fontSize: 14, color: Color(0xff7f7f7f)),
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 16,
-                  ),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 12,
-                    ),
-                    child: Image.asset(
-                      'assets/icons/search7.png',
-                      color: Color(0xff7f7f7f),
-                      width: 20,
-                      height: 20,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  suffixIcon: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 12,
-                    ),
-                    child: Image.asset(
-                      'assets/icons/camera.png',
-                      color: Color(0xff7f7f7f),
-                      width: 20,
-                      height: 20,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  fillColor: Colors.grey.shade200,
-                  filled: true,
-                  focusColor: Colors.black,
-                ),
-              ),
+    return SafeArea(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.20,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/banner.png',
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
             ),
-          ),
-          Positioned(
-            left: 311,
-            top: 50,
-            child: Material(
-              type: MaterialType.transparency,
-              child: InkWell(
-                onTap: () {},
-                child: Ink(
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/icons/bell-ring.png'),
+            Positioned(
+              left: 35,
+              top:30,
+              child: SizedBox(
+                width: 250,
+                height: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter text',
+                    hintStyle: TextStyle(fontSize: 14, color: Color(0xff7f7f7f)),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 16,
                     ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 12,
+                      ),
+                      child: Image.asset(
+                        'assets/icons/search7.png',
+                        color: Color(0xff7f7f7f),
+                        width: 20,
+                        height: 20,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    suffixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 12,
+                      ),
+                      child: Image.asset(
+                        'assets/icons/camera.png',
+                        color: Color(0xff7f7f7f),
+                        width: 20,
+                        height: 20,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    fillColor: Colors.grey.shade200,
+                    filled: true,
+                    focusColor: Colors.black,
                   ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            left: 357,
-              top: 50,
+            Positioned(
+              left: 290,
+              top: 45,
               child: Material(
                 type: MaterialType.transparency,
                 child: InkWell(
                   onTap: () {},
                   child: Ink(
-                    width: 30,
-                    height: 31,
+                    width: 24,
+                    height: 24,
                     decoration: BoxDecoration(
                       image: DecorationImage(
-                        image: AssetImage('assets/icons/comment.png'),
+                        image: AssetImage('assets/icons/bell-ring.png'),
                       ),
                     ),
                   ),
                 ),
               ),
-          ),
-        ],
+            ),
+            Positioned(
+              left: 330,
+                top: 46,
+                child: Material(
+                  type: MaterialType.transparency,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Ink(
+                      width: 24,
+                      height: 24,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/comment.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+            ),
+          ],
+        ),
       ),
     );
   }
